809b5a92d6733c5c76086a3d2237522d5138217c
PatrikSmit <133951086+PatrikSmit@users.noreply.github.com>
2023-05-18 13:44:38 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -8,14 +8,10 @@ int main(void) {
 
 	int pocetak = 1;
 
-	char* datoteka = "kontakti.bin";
-
 	while (pocetak) {
 
 		pocetak = izbornik(datoteka);
 	}
-
-	printf("Kraj programa.");
 
 	return 0;
 }
